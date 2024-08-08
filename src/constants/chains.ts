f38277c5f2f3dab5495e82758c32332b7a45d5a0
thanghoduc <hoducthangtn2nhvt@gmail.com>
2024-08-08 18:21:27 +0700

    chore: contract mainnet

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -1,4 +1,4 @@
 const isDev = process.env.NODE_ENV === 'development'
 
-export const RPC_URL = isDev ? "https://rpc-testnet.viction.xyz" : "https://rpc-testnet.viction.xyz" 
-export const CHAIN_ID = isDev ? 89 : 89
+export const RPC_URL = isDev ? "https://rpc-testnet.viction.xyz" : "https://rpc.viction.xyz" 
+export const CHAIN_ID = isDev ? 89 : 88
