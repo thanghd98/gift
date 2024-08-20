d8cf5f8c6b8d1101aa48daecf102b89cd3de1bc0
thanghoduc <hoducthangtn2nhvt@gmail.com>
2024-08-20 09:44:49 +0700

    feat: setup production

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,7 +1,7 @@
 export const URL_REQUEST = (isDev: boolean) => {
-    return isDev ? 'https://superwallet-chat-api-stg.coin98.tech/adapters/lucky-gift' : ''
+    return isDev ? 'https://superwallet-chat-api-stg.coin98.tech/adapters/lucky-gift' : 'https://superwallet-chat-api.coin98.tech/adapters/lucky-gift'
 }
 
 export const CODE = (isDev: boolean) => {
-    return isDev ? 'dev_adapter9898' : ''
+    return isDev ? 'dev_adapter9898' : 'drofin695292625'
 }
